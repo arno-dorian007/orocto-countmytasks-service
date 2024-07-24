539df01f58aa28bc8f0c7b8a29020fe3198716e1
Ankur Goswami <ankurgoswami584@gmail.com>
2024-07-25 00:16:29 +0530

    Delete DbScripts-Users.sql

--- DbScripts-Users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE UserDetails
-(
-  UserID int primary key not null identity(1, 1),
-  FirstName nvarchar(100) not null,
-  MiddleName nvarchar(100),
-  LastName nvarchar(100),
-  Age int,
-  GenderID int not null
-)
